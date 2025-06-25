08f815a61f4810fd5d2ada5a4708efa73b10e0cb
caioviniciusml <devcaioviniciusml@gmail.com>
2025-06-25 19:56:34 -0300

    feat: adding KY, Fan Tachometer & MicroServo Pins

--- lib/Pins/Pins.h
+++ lib/Pins/Pins.h
@@ -1,7 +1,11 @@
 // SENSORS PINS
 #define PIR_PIN 18
 #define DHT_PIN 19
-#define KY_PIN 34
+#define KY_PIN 32 // GPIO 32 while using ADC Driver
 
 // FAN PINS
-#define FAN_PWM_PIN 25
+#define FAN_PWM_PIN 5
+#define FAN_TACH_PIN 4
+
+// MICRO SERVO PIN
+#define SERVO_PIN 25
