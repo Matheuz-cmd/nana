94013cd54dc3fcf62c10316c128053f398263292
Matheuz-cmd <teteunv@gmail.com>
2025-07-16 11:55:32 -0300

    refactor(Servo Motor): adjust servo swing angle & smoothness

--- lib/MicroServo/MicroServo.cpp
+++ lib/MicroServo/MicroServo.cpp
@@ -2,25 +2,23 @@
 #include "Pins.h"
 
 Servo servo;
-volatile int angle = 90;
 
 void servoSetup()
 {
-  servo.attach(SERVO_PIN);
-  servo.write(angle);
+  servo.attach(SERVO_PIN, 2, 45, 120, 500, 2400, 200);
+
+  if (servo.attached()){
+    Serial.println("[SG 90 SERVO SETUP] Servo Setup Sucessful!");
+  } else {
+    Serial.println("[SG 90 SERVO SETUP] Servo Setup Failed!");
+  }
 }
 
 void startSwing()
 {
-  for (angle = 75; angle <= 105; angle++)
+  for (int angle = 90; angle <= 120; angle++)
   {
     servo.write(angle);
-    delay(40); 
-  }
-
-  for (angle = 105; angle >= 75; angle--)
-  {
-    servo.write(angle);
-    delay(40);
+    delay(20);
   }
 }
